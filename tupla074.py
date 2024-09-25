886a2aa49f964da0f47c39d69769340da7d66197
diovanna melo <diovanna.melo28@gmail.com>
2024-09-25 16:10:49 -0300

    exercicio 074

--- tupla074.py
+++ tupla074.py
@@ -1,3 +1,13 @@
 #crie um programa que gere numeros aleatorios
 #e coloque numa tupla, depois mostre a listagem
 #dos numeros gerados e tb indique o menor e o maior
+import random
+
+tupla_numeros_inteiros_aletorios= tuple(random.randint(1,100) for _ in range(5))
+
+print(f"esses são os numeros da tupla {tupla_numeros_inteiros_aletorios}")
+
+print(f" menor valor da tupla é {min(tupla_numeros_inteiros_aletorios)}")
+
+print(f"o maior valor da tupla é {max(tupla_numeros_inteiros_aletorios)} ")
+
