986f9a7b5fb0020b6a9e98f8ce994c3435473527
diovanna melo <diovanna.melo28@gmail.com>
2024-09-25 18:01:00 -0300

    exercicio 076

---
+++ tupla076.py
@@ -0,0 +1,4 @@
+#crie um programa que tenha uma tupla unica
+# com nomes de produtos e seus respectivos preços, na sequencia
+#no final, mostre uma listagem de preços
+#organizando os dados em forma tabular
