6aecb7112d1cecde74463e47c16c1a916afe1040
diovanna melo <diovanna.melo28@gmail.com>
2024-10-02 17:52:38 -0300

    prova da infinity de listas e tuplas

---
+++ lista080.py
@@ -0,0 +1,5 @@
+#crie um programa onde o usuario possa digitar cinco valores
+#numericos e cadastra-los em uma lista,ja na posição correta
+#de inserção(sem usar o sort()). no final, mostre a lista
+#ordenada na tela.
+
---
+++ prova_infinity_lista_tuplas.py
@@ -0,0 +1,9 @@
+#Crie uma lista contendo seis frutas de sua escolha. Depois de ter a lista pronta,
+#converta essa lista em uma tupla. Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+frutas = ["morango","banana","uva","mertilo","acerola"]
+
+tupla_frutas = tuple(frutas)
+
+print(f"as frutas armazenadas na tupla são: {tupla_frutas}")
+
