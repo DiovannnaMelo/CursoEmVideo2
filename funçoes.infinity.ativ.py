df142454eae51e1e8e9920284407421207ad3b91
diovanna melo <diovanna.melo28@.com>
2024-11-14 19:10:51 -0300

    exercicio 04 e 05 funçoes

---
+++ funçoes.infinity.ativ.py
@@ -0,0 +1,21 @@
+def subtracao (num1, num2):
+    subtracao = num1 - num2
+    return subtracao
+
+n1 = float(input("digite o primeiro numero"))
+n2 = float(input("digite o segundo numero"))
+
+resultado = subtracao (n1, n2)
+
+print(resultado)
+
+def multiplicacao(num1, num2):
+    multiplicacao = num1*num2
+    return multiplicacao
+
+num1 = float(input("digite um numero"))
+num2 = float(input("digite outro numero"))
+
+resultado2 = multiplicacao(num1, num2) 
+
+print(resultado2)
