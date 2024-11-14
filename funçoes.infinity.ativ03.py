96ef40efc184d28e815f746b54b8048e39aeeb7f
diovanna melo <diovanna.melo28@.com>
2024-11-14 19:02:26 -0300

    exercicio 03 funçoes

---
+++ funçoes.infinity.ativ03.py
@@ -0,0 +1,9 @@
+def soma (num1, num2):
+    soma = num1 + num2 
+    return soma
+
+n1 = float(input("digite um numero"))
+n2 = float(input("digite outro numero"))
+
+resultado = soma(n1, n2)
+print(resultado)
