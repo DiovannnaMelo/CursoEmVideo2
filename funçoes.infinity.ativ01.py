209dc5dd5358e0474c89b8f058f12ba386060114
diovanna melo <diovanna.melo28@.com>
2024-11-14 18:36:05 -0300

    exercicio o1 funçoes

---
+++ funçoes.infinity.ativ01.py
@@ -0,0 +1,19 @@
+
+
+def saudacao(nome):
+ #o que esta dentro do () são parametros
+    print(f" olá cara pessoinha {nome} sua linda")
+
+# o que estao nos parenteses quando vc chama a
+#função sao os argumentos
+pessoa = input("digite seu nome ")
+saudacao(pessoa)
+
+
+
+
+
+
+
+
+
